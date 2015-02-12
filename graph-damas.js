7f17701aafe822bd1178783e1a48f041259857ec
Rémy Lalanne <remy@damas-software.org>
2015-02-13 00:29:08 +0100

    initial import

---
+++ graph-damas.js
@@ -0,0 +1,438 @@
+springy_damas = {
+	ray: 6,
+	edge_distance: 6,
+	graph_all_nodes : []
+};
+
+damassvggraph = {
+	getSVG: function() {
+		var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+		svg.setAttributeNS("http://www.w3.org/2000/xmlns/", "xmlns:xlink", "http://www.w3.org/1999/xlink");
+		svg.setAttribute('id', 'svggraph' );
+		//var css = document.createElementNS("http://www.w3.org/2000/svg", "style");
+		//svg.appendChild(css);
+		//css.setAttribute('type', 'text/css' );
+		//css.setAttribute('href', 'graph.css' );
+		var defs = document.createElementNS("http://www.w3.org/2000/svg", "defs");
+		// marker 1
+		var marker = document.createElementNS("http://www.w3.org/2000/svg", "marker");
+		marker.setAttribute('id', 'arrow' );
+		marker.setAttribute('markerWidth', '3' );
+		marker.setAttribute('markerHeight', '3' );
+		marker.setAttribute('refX', '1.5' );
+		marker.setAttribute('refY', '1.5' );
+		marker.setAttribute('orient', 'auto' );
+		marker.setAttribute('markerUnits', 'strokeWidth' );
+		var triangle = document.createElementNS("http://www.w3.org/2000/svg", "path");
+		triangle.setAttribute('d', 'M0,0 L0,3 L3,1.5 Z' );
+		marker.appendChild(triangle);
+		defs.appendChild(marker);
+
+		// marker timealert
+		var marker = document.createElementNS("http://www.w3.org/2000/svg", "marker");
+		marker.setAttribute('id', 'arrowTimealert' );
+		marker.setAttribute('markerWidth', '3' );
+		marker.setAttribute('markerHeight', '3' );
+		marker.setAttribute('refX', '1.5' );
+		marker.setAttribute('refY', '1.5' );
+		marker.setAttribute('orient', 'auto' );
+		marker.setAttribute('markerUnits', 'strokeWidth' );
+		var triangle = document.createElementNS("http://www.w3.org/2000/svg", "path");
+		triangle.setAttribute('d', 'M0,0 L0,3 L3,1.5 Z' );
+		marker.appendChild(triangle);
+		defs.appendChild(marker);
+
+		var g2 = document.createElementNS("http://www.w3.org/2000/svg", "g");
+		var g1 = document.createElementNS("http://www.w3.org/2000/svg", "g");
+		svg.appendChild(defs);
+		svg.appendChild(g2);
+		svg.appendChild(g1);
+		this.defs = defs;
+		this.g2 = g2;
+		this.g1 = g1;
+		return svg;
+	},
+	makeSVGinteractive: function() {
+		// TEST FOR DROP
+		/*
+		allowDrop = function(ev){
+			ev.preventDefault();
+			return false;
+		}
+		drop = function(ev){
+			alert('drop');
+			ev.preventDefault();
+			//console.log(ev.dataTransfer);
+			//console.log(ev.dataTransfer.files[0]);
+			console.log(ev.dataTransfer.files);
+			var files = ev.dataTransfer.files;
+			//alert(files.length);
+			for(i=0;i<files.length;i++)
+			{
+				var file = files[i];
+				var elem = damas.create(file);
+				elem.update({label: file.name });
+				nodes[elem.id] = graph.newNode({'label': file.name});
+				nodes[elem.id].damelem = elem;
+				//console.log(ev.dataTransfer);
+			}
+		}
+		svg.setAttribute('ondragover', 'allowDrop(event)');
+		svg.setAttribute('ondrop', 'drop(event)');
+		*/
+
+		function cancel(e){
+			e.stopPropagation();
+			if(e.preventDefault) e.preventDefault();
+			e.dataTransfer.dropEffect = 'copy';
+			return false; // required by IE
+		}
+		svg.ondragover = cancel;
+		svg.ondragenter = cancel;
+/*
+		svg.ondragover = function(e){
+			e.stopPropagation();
+			e.preventDefault();
+		}
+*/
+		svg.ondragleave = function(e){
+			e.stopPropagation();
+			e.preventDefault();
+		}
+		svg.ondrop = function(e){
+			//alert( e.dataTransfer.getData('Text'));
+			e.stopPropagation();
+			if(e.preventDefault) e.preventDefault();
+			//alert('ondrop');
+			console.log(e.dataTransfer);
+			console.log(e.dataTransfer.files);
+			var files = e.dataTransfer.files;
+
+			// DROP FILES
+			for(i=0;i<files.length;i++)
+			{
+				var file = files[i];
+				var elem = damas.create(file);
+				elem.update({label: file.name });
+				nodes[elem.id] = graph.newNode({'label': file.name});
+				nodes[elem.id].damelem = elem;
+				//console.log(ev.dataTransfer);
+			}
+			console.log(e.dataTransfer.types);
+			var types = e.dataTransfer.types;
+			if(e.dataTransfer.types)
+			{
+				// DROP EXISTING NODE
+				var text = e.dataTransfer.getData('Text');
+				if( text.indexOf(window.location.origin) === 0)
+				{
+					id = text.replace(window.location.origin+window.location.pathname+'#view=', '');
+					var elem = damas.read(parseInt(id));
+                                	Object.extend( elem, damas.element_canvas );
+                                	var img = elem.imageURL();
+                                	nodes[elem.id] = graph.newNode( { 'elem':elem, 'label': elem.label(), 'damid': elem.id, 'damimg': img } );
+                                	nodes[elem.id].damelem = elem;
+				}
+				// DROP LINK
+				else
+				{
+					var elem = damas.create( {
+						url: e.dataTransfer.getData('Text')
+					});
+					nodes[elem.id] = graph.newNode({'label': e.dataTransfer.getData('Text')});
+					nodes[elem.id].damelem = elem;
+				}
+			}
+		}
+		// TEST END
+	}
+}
+
+springy_damas.get_renderer = function( layout)
+{
+	return new Springy.Renderer(layout,
+		function clear() {
+			// code to clear screen
+		},
+		function drawEdge(edge, p1, p2) {
+			if( !edge.shape )
+			{
+				edge.shape = document.createElementNS("http://www.w3.org/2000/svg", 'line');
+				damassvggraph.g1.appendChild( edge.shape );
+				if( edge.source.data.keys.time > edge.target.data.keys.time )
+				{
+					edge.shape.setAttribute('marker-end', 'url(#arrowTimealert)' );
+					edge.shape.setAttribute('class', 'timealert' );
+				}
+				else
+				{
+					edge.shape.setAttribute('marker-end', 'url(#arrow)' );
+				}
+			}
+			var s1 = springy_damas.toScreen(p1); 
+			var s2 = springy_damas.toScreen(p2);
+
+			var theta = Math.atan2(s2.y-s1.y, s2.x-s1.x);
+			var tx1 = s1.x + springy_damas.edge_distance * Math.cos(theta);
+			var ty1 = s1.y + springy_damas.edge_distance * Math.sin(theta);
+
+			var theta = Math.atan2(s1.y-s2.y, s1.x-s2.x);
+			var tx2 = s2.x + springy_damas.edge_distance * Math.cos(theta);
+			var ty2 = s2.y + springy_damas.edge_distance * Math.sin(theta);
+
+			edge.shape.setAttribute('x1', tx1 );
+			edge.shape.setAttribute('y1', ty1 );
+			edge.shape.setAttribute('x2', tx2 );
+			edge.shape.setAttribute('y2', ty2 );
+
+			edge.shape.addEventListener("mouseover", function(){
+				//this.orig_stroke = edge.shape.getAttribute('stroke');
+				//this.setAttribute('stroke', 'green');
+/*
+				if( $('graph_select') )
+				{
+					$('graph_select').update( this.damelem.rightbox() );
+					$('graph_select').setStyle({'opacity': '1'});
+				}
+*/
+			});
+			edge.shape.addEventListener("mouseout", function(){
+				//alert(this.orig_stroke);
+				//this.setAttribute('stroke', this.orig_stroke );
+/*
+				if( $('graph_select') )
+				{
+					$('graph_select').update();
+					$('graph_select').setStyle( {'opacity': '0'});
+				}
+*/
+			});
+		},
+		function drawNode(node, p) {
+			if( !node.shape )
+			{
+				a = document.createElementNS("http://www.w3.org/2000/svg", 'a');
+				a.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '#' + node.data.id );
+				//a.setAttribute('title', escape(JSON.stringify(node.data)));
+				var circle = document.createElementNS("http://www.w3.org/2000/svg", 'circle');
+				//var txt = JSON.stringify(node.data);
+				var title = document.createElementNS("http://www.w3.org/2000/svg", 'title');
+				var titleText = document.createTextNode( node.data.keys.file );
+				title.appendChild(titleText);
+				circle.appendChild(title);
+				node.shape = circle;
+				circle.node = node;
+				circle.point = p;
+				a.appendChild(circle);
+				circle.setAttribute('r',springy_damas.ray);
+				if(node.data.keys.image)
+				{
+					pattern = document.createElementNS("http://www.w3.org/2000/svg", 'pattern');
+					damassvggraph.defs.appendChild( pattern );
+					pattern.setAttribute('id', 'thumb'+node.data.id);
+					pattern.setAttribute('patternContentUnits', 'objectBoundingBox');
+					pattern.setAttribute('x', '0');
+					pattern.setAttribute('y', '0');
+					pattern.setAttribute('width', 1);
+					pattern.setAttribute('height', 1);
+					pattern.setAttribute('preserveAspectRatio', 'xMidYMid slice');
+					var image = document.createElementNS("http://www.w3.org/2000/svg", 'image');
+					pattern.appendChild( image );
+					image.setAttributeNS('http://www.w3.org/1999/xlink', 'xlink:href', node.data.keys.image );
+					image.setAttribute('x', '0');
+					image.setAttribute('y', '0');
+					image.setAttribute('width', '1');
+					image.setAttribute('height', '1');
+					image.setAttribute('preserveAspectRatio', 'xMidYMid slice');
+					circle.setAttribute('fill','url(#thumb'+node.data.id+')');
+				}
+				if(node.data.keys.type)
+				{
+					circle.setAttribute('class', node.data.keys.type);
+				}
+				damassvggraph.g2.appendChild(a);
+
+/*
+				if( node.isselected )
+				{
+					circle.orig_stroke = 'red';
+					circle.setAttribute('stroke', 'red');
+				}
+				if( node.islvl1 )
+				{
+					circle.orig_stroke = 'orange';
+					circle.setAttribute('stroke', 'orange');
+				}
+*/
+				//node.shape.damelem = node.damelem;
+
+/*
+				a.addEventListener("click", function( e ){
+					e.stop();
+					var but = simpleui.accordion( new Element('button').insert('node '+this.getAttribute('href')) );
+					$('content').insert(but);
+					$('content').insert(this.damnode.title());
+					//alert(this.damelem.tooltip());
+					//window.location.hash='#'+this.damelem.id;
+				});
+*/
+				
+/*
+				circle.addEventListener("mouseover", function(){
+					//this.setAttribute('stroke', 'yellow');
+					if( $('graph_select') )
+					{
+						//$('graph_select').update( this.damelem.rightbox() );
+						$('graph_select').setStyle({'opacity': '1'});
+					}
+				});
+				
+				circle.addEventListener("mouseout", function(){
+					//this.setAttribute('stroke', this.orig_stroke );
+					if( $('graph_select') )
+					{
+						$('graph_select').update();
+						$('graph_select').setStyle( {'opacity': '0'});
+					}
+				});
+				
+				circle.addEventListener("mousedown", function(){
+					this.setAttribute('stroke', 'green');
+				});
+*/
+/*
+				circle.addEventListener("mouseup", function(e){
+					//this.setAttribute('stroke', 'yellow');
+					var v = springy_damas.fromScreen( { x: e.pageX, y: e.pageY } );
+					//alert( e.pageX + "," + e.pageY);
+					//alert( this.point.x + "," + this.point.y);
+					//alert( v.x + "," + v.y);
+					//p.x = v.x;
+					//p.y = v.y;
+					//this.point.x = v.x;
+					//this.point.y = v.y;
+					this.point.x = 0;
+					this.point.y = 0;
+					window.springy_renderer.start();
+				});
+*/
+/*
+				circle.addEventListener("mousemove", function( e ){
+					//var v = springy_damas.fromScreen( { x: e.clientX, y: e.clientY } );
+					var v = springy_damas.fromScreen( { x: e.pageX, y: e.pageY } );
+					//this.setAttribute('cx', v.x );
+					//this.setAttribute('cy', v.y );
+					//this.setAttribute('stroke', 'black');
+					this.point.x = v.x;
+					this.point.y = v.y;
+					//e.clientX
+				});
+*/
+/*
+				if( node.damelem.keys.get('type') == 'user' )
+					node.shape.attr('stroke', 'blue');
+				if( node.damelem.keys.get('type') == 'project' )
+					node.shape.attr('fill', 'red');
+				if( node.damelem.keys.get('type') == 'asset' )
+					node.shape.attr('fill', 'orange');
+				if( node.damelem.keys.get('type') == 'asset' )
+					node.shape.attr('fill', 'orange');
+				if( node.damelem.keys.get('type') == 'set' )
+					node.shape.attr('fill', 'lightgreen');
+*/
+
+			}
+			var s = springy_damas.toScreen(p); 
+			node.shape.setAttribute('cx', s.x );
+			node.shape.setAttribute('cy', s.y );
+		}
+	);
+}
+
+springy_damas.toScreen = function(p) {
+	var size = springy_damas.currentBB.topright.subtract(springy_damas.currentBB.bottomleft);
+	var sx = p.subtract(springy_damas.currentBB.bottomleft).divide(size.x).x * 60 + 200;
+	var sy = p.subtract(springy_damas.currentBB.bottomleft).divide(size.y).y * 60 + 200;
+	return new Springy.Vector(sx, sy);
+};
+
+springy_damas.fromScreen = function(s) {
+	var size = springy_damas.currentBB.topright.subtract(springy_damas.currentBB.bottomleft);
+	var px = (s.x / springy_damas.viewport.getBBox().width) * size.x + springy_damas.currentBB.bottomleft.x;
+	var py = (s.y / springy_damas.viewport.getBBox().height) * size.y + springy_damas.currentBB.bottomleft.y;
+	return new Springy.Vector(px, py);
+};
+
+
+
+springy_damas.get_springy_graph = function( id ) {
+	graph = new Springy.Graph();
+	nodes = {};
+	//alert(','.split(id));
+	//alert(typeof(id.split(",")));
+	damas.utils.command_a( { cmd: 'graph', id: id }, function( ret ){
+		var json = JSON.parse(ret.text);
+		$A( json['nodes']).each( function(n){
+			//if(n.id != id)
+			//{
+			var elem = damas.utils.readJSONElement(n);
+			nodes[elem.id] = graph.newNode( { 'label': elem.label() } );
+			nodes[elem.id].damelem = elem; 
+			//}
+			//if( n.id == id )
+			if( id.split(",").indexOf(n.id) !== -1 )
+				nodes[elem.id].isselected = true;
+		});
+		$A( json['links']).each( function(l){
+			if(l['src_id'] == id)
+			{
+				nodes[l['tgt_id']].islvl1 = true;
+				//return;
+			}
+			if(l['tgt_id'] == id)
+				nodes[l['src_id']].islvl1 = true;
+				//return;
+			if(l['tgt_id'] != id && l['src_id'] != id )
+				graph.newEdge( nodes[l['tgt_id']], nodes[l['src_id']] );
+
+		});
+	});
+	return graph;
+}
+
+
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+//springy_damas.circle()
+
+
+
+springy_damas.search_and_draw = function ( keys )
+{
+	damas.search(keys, undefined, undefined, 300, function(indexes){
+		springy_damas.graph_all_nodes = springy_damas.graph_all_nodes.concat(indexes);
+		damas.read(indexes, function(r){
+			r.each( function(elem){
+				if(nodes[elem.id] == undefined)
+				{
+					nodes[elem.id] = graph.newNode( { 'label': elem.label() } );
+					nodes[elem.id].damelem = elem;
+				}
+			});
+		});
+		damas.utils.command_a({cmd:'links2', id: springy_damas.graph_all_nodes.join(',')}, function(r){
+			var json = JSON.parse(r.text);
+			$A(json).each(function(n){
+				graph.newEdge( nodes[n[1]], nodes[n[2]]);
+			});
+		});
+	});
+}
