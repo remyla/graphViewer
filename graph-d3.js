4f4bec7e20f6e652ddc6cc63c3664685e1b244eb
Rémy Lalanne <remy@damas-software.org>
2015-06-10 12:52:56 +0200

    thumbs are unified under a common nodePattern method

--- graph-d3.js
+++ graph-d3.js
@@ -51,7 +51,6 @@
 		this.d3_nodes = [];
 		this.d3_links = [];
 
-
 		this.drag = function()
 		{	d3.behavior.drag()
 			return this.force.drag()
@@ -79,7 +78,6 @@
 		this.svgNodes = d3.select(this.g2).selectAll('g');
 		this.svgLinks = d3.select(this.g1).selectAll('path');
 		this.force.on("tick", this.tick);
-
 
 		this.refreshDebugFrame(htmlelem);
 	}
@@ -386,10 +384,10 @@
 			.attr("style", function(d){ return d.style;});
 		
 		g.append('svg:circle')
-			.attr("id", function(d) { return "thumb"+d._id; })
+			.attr("id", function(d) { return "thumbCirc"+d._id; })
 			.attr("r", 10)
 			.style("fill", function(d) {
-				return "url(#thumbPat"+d._id+")";
+				return "url(#thumb"+d._id+")";
 			})
 			.attr("class", "node");
 //		g.append('svg:image')
@@ -431,32 +429,9 @@
 		
 		var patImage = this.defs.selectAll("pattern")
 			.data(this.nodes)
-			.enter().append('svg:pattern')
-			.attr('patternContentUnits', 'objectBoundingBox')
-			.attr('id', function(d) {
-				return "thumbPat"+d._id;
-			})
-			.attr('x', '0')
-			.attr('y', '0')
-			.attr('width', 1)
-			.attr('height', 1);
-
-		var image = patImage.append('image')
-			.attr('id','thumbnail')
-			.attr('xlink:href', function(d) {
-				return d.image;
-			})
-			.attr('x', '0')
-			.attr('y', '0')
-			.attr('width', 1)
-			.attr('height', 1);
-//			.attr('preserveAspectRatio', 'xMidYMid slice');
-		
-//		image.attr('xlink:href', function(d) {
-//				return  thumbnail(d);
-//			});
-		
-		
+			.enter().append( function(d){
+				return graph.nodePattern(d);
+		});
 
 		g.on("mouseenter", function(d) {
 			graph.force.stop();
